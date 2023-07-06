51051bd854aead0a9b375ad41ff58157f27e221c
Gagakik <gaga.kikvilashvili@gmail.com>
2023-07-06 20:35:15 +0400

    done get data rom home

--- my-app/src/components/Api.js
+++ my-app/src/components/Api.js
@@ -1,20 +1,49 @@
-import  axios from 'axios'
 
 
-const Api = async () => {
-    try {
-      const response = await axios.get('http://213.131.37.250:8889/user/');
-      // Process the response data here
-      console.log(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-    return (
-        <div>
-        
-        </div>
-    )
-  };
+import React, {useEffect, useState} from 'react';
+
+const Api = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('http://213.131.37.250:8889/user/', {
+              headers: {
+                'Content-Type': 'application/json',
+                'Referer':'http://localhost:3000/',
+              },
+            });
+            const userReg = await response.json();
+            setData(userReg);
+            console.log(userReg);
+          } catch (err) {
+            console.error(err);
+          }
+        };
   
-export default Api();
+      fetchData();
+    }, []);
+  
+    return (
+      <div>
+        <h1>Data from Django:</h1>
+        <ul>
+          {data.map((item) => (
+            <li key={item.id}>{item.id}, {item.participant}, {item.tel}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+
+export default Api;
+
+
+
+
+
+
+
+
 
