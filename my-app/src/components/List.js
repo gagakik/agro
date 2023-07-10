821bd1f9ebb8c1741104cd44a94decac2bd5f2c2
Gagakik <gaga.kikvilashvili@gmail.com>
2023-07-10 23:05:56 +0400

    add slide function

--- my-app/src/components/List.js
+++ my-app/src/components/List.js
@@ -1,28 +1,35 @@
-import './list.css'
-
-import React from 'react';
+import React, { useState } from 'react';
+import './list.css';
 
 const List = (props) => {
-    return (
-        <div className='container'>
-        <div className='search'>
-        <label for="gsearch">Search Participants:</label>
-        <input type="psearch" id="psearch" name="psearch"/>
+  const [detal, setDetal] = useState(false);
+
+  const handleToggle = () => {
+    setDetal(!detal);
+  };
+
+  return (
+    <div className='container'>
+      <div className='search'>
+        <label htmlFor="psearch">Search Participants:</label>
+        <input type="text" id="psearch" name="psearch" />
+      </div>
+      <div className='compList'>
+        <div className='Participants'>
+          <p className='num'>1</p>
+          <p className='num'>ltd Participants</p>
         </div>
-        <div className='compList'>
-            <div className='Participants'>
-            <p className='num'>1</p>
-            <p className='num'>ltd Participants</p>
-            </div>
-            <div className='contact'>
-            <p className='num'>Tel:</p>
-            <p className='num'>email:</p>
-            <p className='num'>Contact Person:</p>
-            </div>
-            <button className='detals'>Detals</button>
+        <div className='contact'>
+          <p className='num'>Tel:</p>
+          <p className='num'>email:</p>
+          <p className='num'>Contact Person:</p>
         </div>
-        </div>
-    );
+        <button onClick={handleToggle}>{detal ? 'Turn Off' : 'Turn On'}</button>
+       
+      </div>
+      {detal && <div className='slide'>test</div>}
+    </div>
+  );
 }
 
 export default List;
